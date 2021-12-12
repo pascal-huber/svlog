c37324d63d10577a92978483b9fec34f3389e232
Pascal Huber <pascal.huber@resolved.ch>
2021-12-12 20:42:21 +0100

    add basic log printing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 use clap::{App, load_yaml};
 use glob::glob;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::path::{Path, PathBuf};
+
+static LOG_DIR: &str = "/var/log/socklog/";
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct LogLine {
+    date_str: String,
+    content: String
+}
+
+fn create_logline(line : String) -> LogLine {
+    let date_str : &str = &line[..25];
+    let content_str : &str = &line[26..];
+    LogLine{
+        date_str: date_str.to_string(),
+        content: content_str.to_string(),
+    }
+}
 
 fn list_services(){
     println!("Services:");
-    let path = std::path::Path::new("/var/log/socklog/");
+    let path = Path::new(LOG_DIR);
     for entry in path.read_dir().expect("read_dir call failed")  {
         if let Ok(entry) = entry {
             let p = entry.path();
@@ -13,16 +33,41 @@ fn list_services(){
     }
 }
 
-fn services_files(services: Vec<&str>, files: &mut Vec<std::path::PathBuf>){
+fn service_files(services: Vec<&str>, files: &mut Vec<PathBuf>){
     for service in services {
-        // TODO: move basedir out of the loop
-        let basedir : String = "/var/log/socklog/".to_string();
-        let glb = basedir + service + "/*.[us]";
-        for entry in glob(&glb[..]).expect("Failed to read glob pattern") {
-            if let Ok(path) = entry {
-                files.push(path);
+        for glob_str_ext in ["/current", "/*.[su]"] {
+            let glob_str = String::from(LOG_DIR) + service + glob_str_ext;
+            for entry in glob(&glob_str[..]).expect("Failed to read glob pattern") {
+                if let Ok(path) = entry {
+                    files.push(path);
+                }
             }
         }
+    }
+}
+
+fn extract_loglines(file: PathBuf, loglines: &mut Vec<LogLine>){
+    let file = File::open(file);
+    if let Ok(file) = file {
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            if let Ok(content) = line {
+                let logline = create_logline(content);
+                loglines.push(logline);
+            }
+        }
+    }
+}
+
+fn show_logs(services : Vec<&str>){
+    let mut files : Vec<PathBuf> = Vec::new();
+    service_files(services, &mut files);
+    let mut loglines: Vec<LogLine> = Vec::new();
+    for file in files {
+        extract_loglines(file, &mut loglines);
+    }
+    for logline in loglines {
+        println!("{} {}", logline.date_str, logline.content);
     }
 }
 
@@ -36,16 +81,9 @@ fn main() {
         std::process::exit(0);
     }
 
+    let mut services: Vec<&str> = ["**"].to_vec();
     if matches.is_present("services"){
-
-        let services: Vec<&str> = matches.values_of("services").unwrap().collect();
-        let mut files : Vec<std::path::PathBuf> = Vec::new();
-        services_files(services, &mut files);
-        println!("files:");
-        for x in files {
-            println!(" - {:?}", x);
-        }
-
+        services = matches.values_of("services").unwrap().collect();
     }
-
+    show_logs(services);
 }
