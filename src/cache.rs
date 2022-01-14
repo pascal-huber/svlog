64de215c6833be4038f04b43267935cc116e83d8
Pascal Huber <pascal.huber@resolved.ch>
2022-01-14 21:05:54 +0100

    implement logwatcher and refactoring

---
+++ src/cache.rs
@@ -0,0 +1,69 @@
+use std::collections::VecDeque;
+
+pub struct Cache<T> {
+    list: VecDeque<T>,
+    size: usize,
+}
+
+impl<T: PartialEq> Cache<T> {
+    pub fn new(size: u8) -> Self {
+        Cache {
+            list: VecDeque::new(),
+            size: usize::from(size),
+        }
+    }
+
+    // returns false if item already in list
+    pub fn push(&mut self, item: T) -> bool {
+        let mut result: bool = true;
+        if self.list.contains(&item) {
+            let index = self.list.iter().position(|r| r == &item).unwrap();
+            self.list.remove(index);
+            result = false;
+        }
+        self.list.push_front(item);
+        while self.list.len() > self.size {
+            self.list.pop_back();
+        }
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_item() {
+        let mut stk: Cache<u32> = Cache::new(2);
+        assert!(stk.push(1));
+        assert_eq!(stk.list.len(), 1);
+    }
+
+    #[test]
+    fn add_same_item() {
+        let mut stk: Cache<u32> = Cache::new(2);
+        assert!(stk.push(1));
+        assert!(!stk.push(1));
+        assert_eq!(stk.list.len(), 1);
+    }
+
+    #[test]
+    fn size_limit() {
+        let mut stk: Cache<u32> = Cache::new(2);
+        stk.push(1);
+        stk.push(2);
+        stk.push(3);
+        assert_eq!(stk.list.len(), 2);
+        assert!(stk.push(1)) // i.e. 1 was not in the list
+    }
+
+    #[test]
+    fn test_string() {
+        let mut stk: Cache<String> = Cache::new(2);
+        let item1 = "asdf".to_string();
+        stk.push(item1);
+        let item2 = "asdf".to_string();
+        assert!(!stk.push(item2)); // item wasl already in list
+    }
+}
---
+++ src/logprinter.rs
@@ -0,0 +1,134 @@
+use crate::cache::*;
+use crate::logfile::*;
+use crate::logline::*;
+use crate::util::*;
+
+use calm_io::stdoutln;
+use chrono::NaiveDateTime;
+use notify::{raw_watcher, Op, RawEvent, RecursiveMode, Watcher};
+use rayon::prelude::*;
+use regex::Regex;
+use std::collections::BTreeSet;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::Seek;
+use std::io::SeekFrom;
+use std::path::Path;
+use std::sync::mpsc::channel;
+
+pub struct LogPrinter<'a> {
+    log_files: Vec<LogFile<'a>>,
+    stack: Cache<String>,
+    re: Option<Regex>,
+    jobs: usize,
+    from: Option<NaiveDateTime>,
+    until: Option<NaiveDateTime>,
+}
+
+impl<'a> LogPrinter<'a> {
+    pub fn new(
+        log_files: Vec<LogFile<'a>>,
+        re: Option<Regex>,
+        jobs: usize,
+        from: Option<NaiveDateTime>,
+        until: Option<NaiveDateTime>,
+    ) -> LogPrinter<'a> {
+        let stack: Cache<String> = Cache::new(20);
+        LogPrinter {
+            log_files,
+            stack,
+            re,
+            jobs,
+            from,
+            until,
+        }
+    }
+
+    pub fn jump_to_end(&mut self) {
+        for log_file in &mut self.log_files {
+            log_file.jump_to_end();
+        }
+    }
+
+    pub fn print_logs(&mut self) {
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(self.jobs)
+            .build_global()
+            .unwrap();
+
+        // TODO: can those 3 copies/clones be avoided?
+        let fc = self.from;
+        let uc = self.until;
+        let rc = self.re.clone();
+        let loglines: BTreeSet<LogLine> = self
+            .log_files
+            .par_iter_mut()
+            .flat_map(|f| f.extract_loglines(fc, uc, &rc).into_par_iter())
+            .collect();
+
+        #[allow(unused_must_use)]
+        for logline in loglines {
+            stdoutln!("{}", logline);
+        }
+    }
+
+    pub fn watch_logs(&mut self) {
+        let (tx, rx) = channel();
+        let mut watcher = raw_watcher(tx).unwrap();
+        watcher.watch(LOG_DIR, RecursiveMode::Recursive).unwrap();
+        loop {
+            let (path, event) = match rx.recv() {
+                Ok(RawEvent {
+                    path: Some(path),
+                    op: Ok(op),
+                    cookie: _,
+                }) => (Some(path), Some(op)),
+                Ok(_) => (None, None),
+                Err(_) => (None, None),
+            };
+            if let Some(e) = event {
+                // TODO: should I handle Op::CLOSE_WRITE or Op::WRITE?
+                // println!("{:?} on {:?}", e, path);
+                if e == Op::WRITE {
+                    self.handle_event(&path.unwrap());
+                }
+            }
+        }
+    }
+
+    fn handle_event(&mut self, path: &Path) {
+        // TODO: refactor this hard
+        for entry in &mut self.log_files {
+            let path_name = path.to_str();
+            if let Some(path_name) = path_name {
+                if entry.name == path_name {
+                    let file = File::open(&path);
+                    if let Ok(file) = file {
+                        let meta = file.metadata();
+                        let length = meta.unwrap().len();
+                        if length >= entry.position {
+                            let mut reader = BufReader::new(file);
+                            let res = reader.seek(SeekFrom::Start(entry.position));
+                            if res.is_ok() {
+                                for line in reader.lines().flatten() {
+                                    if self.stack.push(String::from(&line)) {
+                                        let logline = LogLine::new(line);
+                                        if logline.is_match(&self.re) {
+                                            println!("{}", logline);
+                                        }
+                                    }
+                                }
+                                entry.position = length;
+                            }
+                        } else {
+                            entry.position = 0;
+                            self.handle_event(path);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
